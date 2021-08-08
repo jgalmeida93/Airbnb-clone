36ec86eee4293795e6d2c04361dc6fb27ff78e41
Jonas Gabriel <jgalmeida1993@gmail.com>
2021-08-08 17:46:02 -0300

    add mapbox_key

--- next.config.js
+++ next.config.js
@@ -2,4 +2,7 @@ module.exports = {
   images: {
     domains: ["links.papareact.com"],
   },
+  env: {
+    mapbox_key: 'pk.eyJ1IjoiamdhbG1laWRhOTMiLCJhIjoiY2tzM21mcGU2MDB5NjJwcWxmdG5tNXA5YSJ9.t3b6iq8KgH-FX3uy73hevQ'
+  }
 };
