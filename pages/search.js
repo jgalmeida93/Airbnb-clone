75282c192ff488546d22cdaa5cf530ed3a2aad82
Jonas Gabriel <jgalmeida1993@gmail.com>
2021-08-08 17:45:57 -0300

    change breakpoint from md to xl

--- pages/search.js
+++ pages/search.js
@@ -54,7 +54,7 @@ function Search({ searchResults }) {
           </div>
         </section>
 
-        <section className="hidden md:inline-flex md:min-w-[600px]">
+        <section className="hidden xl:inline-flex xl:min-w-[600px]">
           <Map searchResults={searchResults} />
         </section>
       </main>
